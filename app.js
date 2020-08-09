7fa87f50ea43239a9da6311ae18cf01d32621d62
Colin Campbell <colin@elusivelabs.com>
2020-08-09 19:34:20 -0400

    Add next game lookup

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	// next game
+	$.getJSON( "https://statsapi.web.nhl.com/api/v1/teams/10?expand=team.schedule.next ", function( data ) {
+		// console.log(data);
+
+		let nextGame = data.teams[0].nextGameSchedule.dates[0];
+		// console.log(nextGame)
+		$("div.nextGameDate").html(moment(nextGame.date).format('LL'));
+		$("div.nextGameHome").html(nextGame.games[0].teams.home.team.name);
+		$("div.nextGameAway").html(nextGame.games[0].teams.away.team.name);
+	});    
+});
